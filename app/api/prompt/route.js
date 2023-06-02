99e199c43ef3f5b2ba659f63dbac5eaab24931f6
muhammadnadeem <muhammad.nadeem@techoverflow.io>
2023-06-02 16:24:26 +0500

    another-issue-resolved

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,13 @@
 import Prompt from '@models/prompt';
 import { connectToDB } from '@utils/database';
 
-export const GET = async (req, res) => {
+export const GET = async (request) => {
     try {
         await connectToDB();
         const allPrompts = await Prompt.find({}).populate('creator');
 
         return new Response(JSON.stringify(allPrompts), { status: 200 });
     } catch (err) {
-        return new Response(err.message, { status: 500 });
+        return new Response('Failed to fetch prompts', { status: 500 });
     }
 };
